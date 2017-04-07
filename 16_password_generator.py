9be3be425ec43bca6d9c53e7d4d9a1ea81dfcab6
obitech <the.am0k@gmx.de>
2017-04-07 10:56:45 +0200

    shorter script

--- 16_password_generator.py
+++ 16_password_generator.py
@@ -11,65 +11,16 @@ Extra:
 import random
 
 # Pick a strength: 1 = weak, 2 = mid, 3 = strong
-def generate_password(strength=2):
+def generate_password(length=5):
 
-    if (strength < 1) or (strength > 3):
-        print("Please enter a strength between 1 and 3 (inclusively)")
+    if length == 0:
         return None
 
-    pool = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-    1,2,3,4,5,6,7,8,9,0,
-    '!', '@', '#', '$', '%', '&', '*', '(', ')', '_', '=', '-', '+']
+    pool = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?+=-_[]"
 
-    password = []
+    return ''.join([random.choice(pool) for x in range(length)])
 
-    # Determining length of password
-    if strength == 1:
-        length = random.randint(5, 10)
-    elif strength == 2:
-        length = random.randint(11, 20)
-    else:
-        length = random.randint(21, 50)
 
-    # Randomly pick from pool
-    for i in range(length):
-        char = random.choice(pool)
-
-        # Randomize upper/lower
-        if type(char) == str and char.isalpha():
-            if random.randint(0, 1) == 1:
-                char = char.upper()
-
-        password.append(char)
-
-    # Randomize for good measure
-    for i in range(10):
-        random.shuffle(password)
-
-    # print("Returning: " + str(password))
-    return ''.join(str(x) for x in password if x != type(str))
-
-def test_weak(n=5):
-    print("Weak passwords:")
-
-    for i in range(n):
-        print(str(generate_password(1)))
-    print()
-
-def test_mid(n=5):
-    print("Mid passwords:")
-
-    for i in range(n):
-        print(str(generate_password(2)))
-    print()
-
-def test_strong(n=5):
-    print("Strong passwords:")
-
-    for i in range(n):
-        print(str(generate_password(3)))
-    print()
-
-test_weak(10)
-test_mid(10)
-test_strong(10)
+# Test
+for i in range(10):
+    print(generate_password(random.randint(0, 20)))
